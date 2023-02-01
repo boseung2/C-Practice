32ed0cdd3382e086b95a1640dc182e3a1b03801c
boseung2 <jungbs3726@naver.com>
2023-02-02 05:50:58 +0900

    singly-linked-list GetNodeAt함수 추사

--- Data-Structure/singly-linked-list.c
+++ Data-Structure/singly-linked-list.c
@@ -25,13 +25,15 @@ void SLL_AppendNode(Node** Head, Node* NewNode) {
   // Head가 없다면 NewNode를 Head로 지정한다.
   if((*Head) == NULL) {
     *Head = NewNode;
-  } 
+  }
   // Head가 있다면 Tail의 NextNode를 NewNode로 지정한다.
   else {
     Node* Tail = (*Head);
+
     while(Tail->NextNode != NULL) {
       Tail = Tail->NextNode;
     }    
+
     Tail->NextNode = NewNode;
   }
 }
@@ -40,7 +42,7 @@ Node* SLL_GetNodeAt(Node* Head, int Location) {
   Node* Current = Head;
   
   while(Current != NULL && (--Location) >= 0) {
-    Current =Current->NextNode;
+    Current = Current->NextNode;
   }
 
   return Current;
@@ -48,13 +50,9 @@ Node* SLL_GetNodeAt(Node* Head, int Location) {
 
 int main() {
   Node* List = NULL;
-  Node* NewNode = NULL;
 
-  NewNode = SLL_CreateNode(117);
-  SLL_AppendNode(&List, NewNode);
-
-  NewNode = SLL_CreateNode(119);
-  SLL_AppendNode(&List, NewNode);
+  SLL_AppendNode(&List, SLL_CreateNode(117));
+  SLL_AppendNode(&List, SLL_CreateNode(119));
 }
 
 // sizeof(Node) : 16
